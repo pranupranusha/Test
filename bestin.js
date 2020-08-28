ab53c1487a1484529909b1d9088afda1b0ec2eb0
pranupranusha <pranupranusha.com>
2020-08-28 21:24:16 +0530

    First Commit

---
+++ bestin.js
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>	Bestin</title>
+<style>
+#div {
+	padding-left: 365px;
+	font-size: 35px;
+}
+#main {
+	padding-left: 365px;
+	font-size:20px;
+	padding-top: 15px;
+}
+#div1 {
+	float:left;
+	display: inline-block;
+	padding-left: 365px;
+	font-size:10px;
+	padding-top: 15px;
+
+}
+#div2 {
+	float:left;
+	display: inline-block;
+	padding-left: 20px;
+	font-size:10px;
+	padding-top: 15px;
+}
+#div3 {
+	float:left;
+	display: inline-block;
+    padding-left: 15px;
+    padding-top: 15px;
+	font-size:10px;
+}
+#div4 {
+	float:left;
+	 display: inline-block; 
+	 padding-left: 20px;
+	 padding-top: 15px;
+	font-size:10px;
+
+}
+#main1 {
+	float:left;
+	display: inline-block; 
+	padding-left: 360px;
+	padding-top: 15px;
+	font-size:20px;
+}
+
+</style>
+</head>
+<body>
+<div id="div"><i>Bestin here</i></div>
+<hr>
+<br>
+<br>
+<br>
+<br>
+<div id="main">Find the best <select>
+<option value="places">places</option>
+<option value="Restaurants">Restaurants</option>
+<option value="Hotels">Hotels</option>
+<option value="Things to do">Things to do</option>
+<option value="Street food">Street food</option>
+<option value="Tourists spot">Tourists spots</option>
+<option value="Shopping spots">Shopping spots</option>
+<option value="All places">All places</option>
+</select>in location
+</div>
+<br>
+
+<div id="main">Trending Places<span style="padding-left:440px";>See More</span></div>
+<br>
+
+<div>
+<div id="div1">
+
+<div><img src="C:\Users\nagap\Downloads\Image.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+</div>
+
+<div id="div2">
+<div><img src="C:\Users\nagap\Downloads\Image 1.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+<div id="div3">
+<div><img src="C:\Users\nagap\Downloads\Image 3.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+<div id="div4">
+<div><img src="C:\Users\nagap\Downloads\Image 4.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div><br>
+
+
+<div id="main1">Trending Places<span style="padding-left:440px;">See More</span></div>
+<br>
+
+<div>
+<div id="div1">
+<div><img src="C:\Users\nagap\Downloads\Image 3.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+<div id="div2">
+<div><img src="C:\Users\nagap\Downloads\Image 1.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+<div id="div3">
+<div><img src="C:\Users\nagap\Downloads\Image.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+</div>
+
+<div id="div4">
+<div><img src="C:\Users\nagap\Downloads\Image 4.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+<div id="main1">Trending Places<span style="padding-left:440px;">See More</span></div>
+<br>
+
+<div>
+<div id="div1">
+<div><img src="C:\Users\nagap\Downloads\Image 3.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+<div id="div2">
+<div><img src="C:\Users\nagap\Downloads\Image 1.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+<div id="div3">
+<div><img src="C:\Users\nagap\Downloads\Image.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+</div>
+
+<div id="div4">
+<div><img src="C:\Users\nagap\Downloads\Image 4.jpg" style="width:100px;height:100px"></div>
+	
+<div><p>Chowmahalla palace</p></div>
+<div><p>History Museum</p></div>
+<div><p>Architectural styles. Monuments . Nizam</p></div>
+
+</div>
+
+</div>
+
+	
+
+	
+	
+
+
+	 
+
+
+
+
+
+
+
+
+
+
+    
+
+
+   
+
+
+
+
+
+
+
+</body>
+</html>
